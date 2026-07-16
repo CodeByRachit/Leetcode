d04bd71c568784c89ec85961cf613db9c54cb545
RACHIT YADAV <rachit4yadav40@gmail.com>
2026-07-16 23:28:12 +0530

    Time: 80 ms (33.95%), Space: 155.6 MB (87.1%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
+    int Gcd(int a, int b) {
+        while (b != 0) {
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+        return a;
+    }
     long long gcdSum(vector<int>& nums) {
-        int n =nums.size();
-        vector<int> prevGcd(n);
-        prevGcd[0]=nums[0];
-        int  mx=nums[0];
-        for(int i =1;i<n ;i++){
-            mx=max(mx,nums[i]);
-            long long gcd =__gcd(mx,nums[i]);
-            prevGcd[i]=gcd ;
+        int n = nums.size();
+        vector<int> prefixGcd(n);
+        prefixGcd[0] = nums[0];
+        int mx = nums[0];
+        for (int i = 1; i < n; i++) {
+            mx = max(mx, nums[i]);
+            prefixGcd[i] = Gcd(mx, nums[i]);
         }
-        sort(prevGcd.begin(),prevGcd.end());
-        int i =0;
-        int j =n-1 ;
-        long long sum =0;
-        while(i<j){
-           
-           sum+=__gcd(prevGcd[i++],prevGcd[j--]);
+        sort(prefixGcd.begin(), prefixGcd.end());
+        long long sum = 0;
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+            sum += Gcd(prefixGcd[i], prefixGcd[j]);
+            i++;
+            j--;
         }
-        return sum ;
-        return 0;    
+        return sum;
     }
 };
